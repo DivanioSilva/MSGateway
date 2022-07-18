6f6ae38f5e138a9b5111e3f29c489ae9ab2203b4
Divanio Silva <dcsilva@mail.ru>
2022-07-19 00:32:49 +0200

    Config some routers

---
+++ src/main/java/com/ds/gateway/msgateway/config/GatewayRouting.java
@@ -0,0 +1,20 @@
+package com.ds.gateway.msgateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayRouting {
+    private static final String MS_ADDRESS_LABEL = "lb://ms-address";
+
+    @Bean
+    public RouteLocator configureRoute(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("country", r->r.path("/country/**").uri(MS_ADDRESS_LABEL)) //static routing
+                .route("state", r->r.path("/state/**").uri(MS_ADDRESS_LABEL)) //dynamic routing
+                .route("city", r->r.path("/city/**").uri(MS_ADDRESS_LABEL))
+                .build();
+    }
+}
